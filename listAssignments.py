e6e7cfe59c152857a41f25a9b8b5abd690f58936
Di <53130591+rdsdi@users.noreply.github.com>
2022-04-01 11:58:45 -0700

    edited and formatted comments

--- listAssignments.py
+++ listAssignments.py
@@ -1,50 +1,31 @@
-# import datefinder
+# This is a program that receives a text input of a list of assignments from a syllabus
+# and outputs a formatted list of assignments that can be pasted into Todoist
 
-# input_string = "3 February 2022"
-# # a generator will be returned by the datefinder module. I'm typecasting it to a list. Please read the note of caution provided at the bottom.
-# matches = list(datefinder.find_dates(input_string))
-
-# date = matches[0]
-# print(date)
-
-import csv
 import dateutil.parser as dparser
 from dateutil.parser import ParserError
 import re
 
-header = ['date', 'name', 'time']
+# number of lines to combine
 combine = 1
 
-# open the file in the write mode
+# open the given file in read mode
 with open('info200hw.txt', 'r') as input:
-
-    #with open('sampleOutput.csv', 'w') as output:
-        # create the csv writer
-        #writer = csv.writer(output)
-
-        # write header to the csv file
-        #writer.writerow(header)
-
-        # prints datetime, extraneous information on separate lines
     for line in input:
         # combine given number of lines
         for x in range(combine - 1):
             line = (line, next(input))
             line = '  '.join(line)
 
-        # split line at multiple spaces, keyword 'due'
+        # split line at multiple spaces, keyword 'due', or tab
         lineList = re.split('\\s{2,}|due|\\t', line.strip())
 
+        # move time to beginning of list
         for x in range(len(lineList)):
-            try:
-                # move time to beginning of tuple
-                if re.search(r"\d[ap]m", lineList[x]):
-                    lineList.insert(0, lineList.pop(x).strip())
-                    lineList[0] = ' '.join((lineList[0], lineList.pop(1)))
-            except IndexError:
-                print("ERROR, NEXT")
+            if re.search(r"\d[ap]m", lineList[x]):
+                lineList.insert(0, lineList.pop(x).strip())
+                lineList[0] = ' '.join((lineList[0], lineList.pop(1)))
 
-        # None same as null
+        # prints date, time, and assignment name on the same line in that order
         dt = None
         for item in lineList:
             if dt == None:
@@ -54,11 +35,6 @@ with open('info200hw.txt', 'r') as input:
                 except ParserError:
                     pass
             else:
+                # removes unwanted words
                 print(item.replace('Assignment ',''), end = '')
-
         print('')
-
-            # # write a row to the csv file
-            # dt, tokens = dparser.parse(line, fuzzy_with_tokens=True)
-            # print(tokens)
-            # print(dt)
